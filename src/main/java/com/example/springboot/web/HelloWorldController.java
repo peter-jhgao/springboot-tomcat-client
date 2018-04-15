5889720ddabf30ca662e06a18db308930a7c08e6
peter-jhgao <29678838@qq.com>
2018-04-15 15:32:45 +0800

    Create HelloWorldController.java

---
+++ src/main/java/com/example/springboot/web/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello, CCE!";
+    }
+}
